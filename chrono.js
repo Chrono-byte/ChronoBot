bc3694789c19111d5e86a5abee0bcbb77cfa7209
TJDoesCode <tjdoescode@gmail.com>
2017-07-15 18:57:45 -0500

    add new comando core

---
+++ chrono.js
@@ -0,0 +1,93 @@
+//eslint-disable-next-line
+const config = require('./config.json');
+const commando = require('discord.js-commando');
+const client = new commando.Client({
+  owner: ['251383432331001856'],
+  commandPrefix: config.prefix,
+  unknownCommandResponse: false
+});
+//const defclient = new Discord.Client();
+const path = require('path');
+const sqlite = require('sqlite');
+const oneLine = require('common-tags').oneLine;
+console.log('Requires initialized.');
+
+client.registry
+  .registerGroups([
+    ['general', 'general'],
+    ['misc', 'Miscellaneous'],
+    ['fun', 'Fun']
+  ])
+
+  .registerDefaults()
+
+  .registerCommandsIn(path.join(__dirname, 'commands'));
+
+client.setProvider(sqlite.open(path.join(__dirname, 'settings.sqlite3')).then(db => new commando.SQLiteProvider(db))).catch(console.error);
+console.log('Commando set up.');
+console.log('Awaiting log in.');
+
+client
+  .on('error', () => console.error)
+  .on('warn', () => console.warn)
+  .on('debug', () => console.log)
+  .on('ready', () => {
+    console.log(`Client ready; logged in as ${client.user.tag} (${client.user.id}) with prefix "${config.prefix}"`)
+    console.log('Awaiting actions.')
+  })
+  .on('disconnect', () => console.warn('Disconnected!'))
+  .on('reconnecting', () => console.warn('Reconnecting...'))
+  .on('commandError', (cmd, err) => {
+    if (err instanceof commando.FriendlyError) return;
+    console.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
+  })
+  .on('commandBlocked', (msg, reason) => {
+    console.log(oneLine `
+			Command ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ''}
+			blocked; ${reason}
+		`);
+  })
+  .on('commandPrefixChange', (guild, prefix) => {
+    console.log(oneLine `
+			Prefix ${prefix === '' ? 'removed' : `changed to ${prefix || 'the default'}`}
+			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+		`);
+  })
+  .on('commandStatusChange', (guild, command, enabled) => {
+    console.log(oneLine `
+			Command ${command.groupID}:${command.memberName}
+			${enabled ? 'enabled' : 'disabled'}
+			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+		`);
+  })
+  .on('groupStatusChange', (guild, group, enabled) => {
+    console.log(oneLine `
+			Group ${group.id}
+			${enabled ? 'enabled' : 'disabled'}
+			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+		`);
+  })
+  .on('commandRun', (command, promise, msg) => {
+    if (msg.guild) {
+      console.log(`Command ran
+        Guild: ${msg.guild.name} (${msg.guild.id})
+        Channel: ${msg.channel.name} (${msg.channel.id})
+        User: ${msg.author.tag} (${msg.author.id})
+        Command: ${command.groupID}:${command.memberName}
+        Message: "${msg.content}"`)
+    } else {
+      console.log(`Command ran:
+        Guild: DM
+        Channel: N/A
+        User: ${msg.author.tag} (${msg.author.id})
+        Command: ${command.groupID}:${command.memberName}
+        Message: "${msg.content}"`)
+    }
+  })
+
+
+client.login(config.token).catch(console.error);
+
+process.on('unhandledRejection', err => {
+  console.error('Uncaught Promise Error: \n' + err.stack);
+});
