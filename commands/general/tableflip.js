835a5328babe0fd1ff1997f309f803dfffcbd045
TJDoesCode <tjdoescode@gmail.com>
2017-07-15 20:18:51 -0500

    more ASCII fun commands

---
+++ commands/general/tableflip.js
@@ -0,0 +1,24 @@
+//eslint-disable-next-line
+const commando = require('discord.js-commando');
+const oneLine = require('common-tags').oneLine;
+
+module.exports = class TableflipCommand extends commando.Command {
+  constructor(bot) {
+    super(bot, {
+      name: 'tableflip',
+      aliases: ['rage'],
+      group: 'general',
+      memberName: 'tableflip',
+      description: 'Sends the ASCII tableflip.',
+      details: oneLine `
+      Feelin ANGERY? Use this command to flip a table!
+			`,
+      examples: ['shrug']
+    });
+  }
+
+  //eslint-disable-next-line class-methods-use-this
+  async run(message) {
+    message.edit('(╯°□°）╯︵ ┻━┻');
+  }
+};
---
+++ commands/general/unflip.js
@@ -0,0 +1,24 @@
+//eslint-disable-next-line
+const commando = require('discord.js-commando');
+const oneLine = require('common-tags').oneLine;
+
+module.exports = class UnflipCommand extends commando.Command {
+  constructor(bot) {
+    super(bot, {
+      name: 'unflip',
+      aliases: ['revserseflip'],
+      group: 'general',
+      memberName: 'unflip',
+      description: 'Sends the ASCII table unflip.',
+      details: oneLine `
+      Feelin unangery? Use this command to unflip a table.
+			`,
+      examples: ['shrug']
+    });
+  }
+
+  //eslint-disable-next-line class-methods-use-this
+  async run(message) {
+    message.edit('┬─┬﻿ ノ( ゜-゜ノ)');
+  }
+};
