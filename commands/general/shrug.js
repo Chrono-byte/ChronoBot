87ebfe4982023a952beb68ebab404d1e3f39a028
TJDoesCode <tjdoescode@gmail.com>
2017-07-15 19:05:28 -0500

    add fisrt command

---
+++ commands/general/shrug.js
@@ -0,0 +1,24 @@
+//eslint-disable-next-line
+const commando = require('discord.js-commando');
+const oneLine = require('common-tags').oneLine;
+
+module.exports = class ShrugCommand extends commando.Command {
+  constructor(bot) {
+    super(bot, {
+      name: 'shrug',
+      aliases: ['meh'],
+      group: 'general',
+      memberName: 'shrug',
+      description: 'Sends the ASCII shrug.',
+      details: oneLine `
+      Feelin kinda meh? Use this command to shrug.
+			`,
+      examples: ['shrug']
+    });
+  }
+
+  //eslint-disable-next-line class-methods-use-this
+  async run(message) {
+    message.edit('¯\\_(ツ)_/¯');
+  }
+};
