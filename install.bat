b9441e7bd028fd03e2342302e814d95ce9b0a185
TJ The Developer <tjdoescode@gmail.com>
2017-08-11 02:22:24 -0500

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,5 +1,26 @@
 @echo off
 color a
-title Selfbot Installation
+title ChronoBot Installation
+
+echo --------------------------------------------------
+echo Welcome to ChronoBot, a simple Discord Selfbot by Chronomly and TJDoesCode!
+echo This script is to set up the bot.
+echo If you have already ran this script, you do not need to again.
+echo To run the bot, run bot.bat.
+-------------------------------------------------------
+
+call npm -version >NUL 2>&1
+if not "%ERRORLEVEL%" == "0" goto nodeErr
+
+:nodeErr
+echo ---------------------------------------------------
+echo NodeJS was not found. Please install it.
+echo https://nodejs.org
+echo Be sure to install the Current version, NOT the LTS version.
+echo The correct version is the button on the right side.
+echo ---------------------------------------------------
+
+
+echo ---------------------------------------------------
 echo Installing packages...
-CMD npm install
+call npm install
